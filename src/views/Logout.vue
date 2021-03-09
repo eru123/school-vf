a9cd7c0f39bfe2a2ec41eb764c22487f44636cfe
eru123 <39824083+eru123@users.noreply.github.com>
2021-03-09 18:52:43 +0800

    added: logout

---
+++ src/views/Logout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="lds-cnt">
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="lds-msg">Logging out<i></i><i></i><i></i></div>
+  </div>
+</template>
+<script>
+import firebase from "@/firebase";
+
+export default {
+  name: "Logout",
+  created() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.$router.push({ name: "Auth" });
+      })
+      .catch((e) => {
+        console.log("[LOGOUT] Error:", e);
+        this.$router.push({ name: "Auth" });
+      });
+  },
+};
+</script>
