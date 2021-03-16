d521a4517f183165066118f20c0aa771f3637e0e
eru123 <39824083+eru123@users.noreply.github.com>
2021-03-17 00:57:27 +0800

    Deploy

---
+++ docs/precache-manifest.ae5f7a3501a0908b3c8c07da3f0b199c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/school-vf/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/school-vf/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/school-vf/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/school-vf/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/school-vf/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "42d8e75a4b7ac350c3538a793da77270",
+    "url": "/school-vf/index.html"
+  },
+  {
+    "revision": "4f1d431b1e57539dbd28",
+    "url": "/school-vf/js/app.ccf84ec2.js"
+  },
+  {
+    "revision": "3e0162eadb8d851e35fb",
+    "url": "/school-vf/js/chunk-2d0e266c.b9b1f9a9.js"
+  },
+  {
+    "revision": "0f273bdcb5b3bb27b730",
+    "url": "/school-vf/js/chunk-2d215fa4.a68e85d9.js"
+  },
+  {
+    "revision": "e3cc2dc61f686a7e4f02",
+    "url": "/school-vf/js/chunk-3af5782c.dd7cb4c3.js"
+  },
+  {
+    "revision": "3d860bb2cc9e279f460c",
+    "url": "/school-vf/js/chunk-43180e8f.634d1b38.js"
+  },
+  {
+    "revision": "b8a408ed316b6c137708",
+    "url": "/school-vf/js/chunk-a1ea2e2c.fa085bdd.js"
+  },
+  {
+    "revision": "17797460f7d48180463f",
+    "url": "/school-vf/js/chunk-d3c19e98.69bff97a.js"
+  },
+  {
+    "revision": "a20bbf896bb05024fc1bd816166697ad",
+    "url": "/school-vf/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/school-vf/robots.txt"
+  }
+]);
