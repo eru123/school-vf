a0871572a3b2ae9e2bbb6795a37d9ddd80582d4a
eru123 <39824083+eru123@users.noreply.github.com>
2021-03-21 19:20:19 +0800

    remove: settings dialog

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,6 @@
 <template>
   <header class="base-header-component">
     <div>{{ title }}</div>
-    <dialog v-if="settingsTab">
-      <ul>
-        <header>
-          <Icon name="mdi-arrow-left" @click="GlobalButtonCloseSettingsTab" />
-          Settings <small>Menu</small>
-        </header>
-        <li>one</li>
-        <li>two</li>
-        <li @click="GlobalButtonLogout">
-          <Icon name="mdi-logout" />Logout <span></span>
-        </li>
-      </ul>
-    </dialog>
     <div>
       <slot></slot>
     </div>
@@ -24,7 +11,7 @@ import "@/styles/header.stylus";
 export default {
   name: "Header",
   props: {
-    title: String
-  }
+    title: String,
+  },
 };
 </script>
