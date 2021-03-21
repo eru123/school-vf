b8da735122827972ac66aa3350160cdcbcc3502f
eru123 <39824083+eru123@users.noreply.github.com>
2021-03-21 17:44:08 +0800

    optimized: for offline first approach

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,49 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
+
+import Auth from "@/views/Auth.vue"
+import Home from "@/views/Home.vue"
+import Logout from "@/views/Logout.vue"
+import Settings from "@/views/Settings.vue"
+import Account from "@/views/Account.vue"
+import VerifyEmail from "@/views/VerifyEmail.vue"
+import InternalErrorPage from "@/views/InternalError.vue"
+
 const routes = [
   {
     path: "/",
     name: "Auth",
-    component: () => import("@/views/Auth.vue")
+    component: Auth
   },
   {
     path: "/home",
     name: "Home",
-    component: () => import("@/views/Home.vue")
+    component: Home
   },
   {
     path: "/logout",
     name: "Logout",
-    component: () => import("@/views/Logout.vue")
+    component: Logout
   },
   {
     path: "/settings",
     name: "Settings",
-    component: () => import("@/views/Settings.vue")
+    component: Settings
   },
   {
     path: "/account",
     name: "Account",
-    component: () => import("@/views/Account.vue")
+    component: Account
   },
   {
     path: "/verify_email",
     name: "VerifyEmail",
-    component: () => import("@/views/VerifyEmail.vue")
+    component: VerifyEmail
   },
   {
     path: "/error/500",
     name: "InternalError",
-    component: () => import("@/views/InternalError.vue")
+    component: InternalErrorPage
   }
 ];
 
