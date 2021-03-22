edd264c7b5de85a2cedeb0230272d7cb37e8ef8c
eru123 <39824083+eru123@users.noreply.github.com>
2021-03-22 18:59:01 +0800

    added: account page

---
+++ src/components/CoverAndProfile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cover-n-profile">
+    <div class="cover-photo">
+      <img :src="acc.coverPhoto" />
+    </div>
+    <div class="default-photo">
+      <img :src="acc.defaultPhoto" />
+      <div class="upload-btn">Click ME</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/styles/cover-n-profile.stylus";
+export default {
+  name: "CoverAndProfile",
+  data: () => ({
+    acc: {
+      defaultPhoto:
+        "https://www.sketchappsources.com/resources/source-image/profile-illustration-gunaldi-yunus.png",
+      coverPhoto:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThxCQsn2Ft-OxHWzhsJACnYisCZy7mOb4Olw&usqp=CAU"
+    }
+  })
+};
+</script>
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,15 +1,157 @@
 <template>
   <AuthHeader />
-  <div class="settings-page"></div>
+  <Cover />
+  <div class="settings-page">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in
+      beatae voluptas, ex sit voluptatum similique omnis perspiciatis at dolores
+      dolorem. Qui eos necessitatibus est distinctio, sunt ex nihil alias.
+    </p>
+  </div>
 </template>
 
 <script>
-import AuthHeader from "@/components/AuthenticatedHeader.vue";
 import "@/styles/account-page.stylus";
+import Cover from "@/components/CoverAndProfile.vue";
 export default {
   name: "Account",
   components: {
-    AuthHeader
+    Cover
   }
 };
 </script>
